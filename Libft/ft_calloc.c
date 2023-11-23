6bb3d0dafb50d3429e62261588d106658dd66470
Es-salhi Mohammed Amine <117828513+mes-salh@users.noreply.github.com>
2023-11-23 18:03:41 +0100

    update

--- Libft/ft_calloc.c
+++ Libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: mes-salh <mes-salh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 13:46:16 by mes-salh          #+#    #+#             */
-/*   Updated: 2023/11/18 16:10:54 by mes-salh         ###   ########.fr       */
+/*   Updated: 2023/11/22 16:44:10 by mes-salh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ void	*ft_calloc(size_t count, size_t size)
 	char	*ptr;
 	size_t	i;
 
-	i = count * size;
-	if (i / size != count)
+	if ((int)count < 0 || (int)size < 0)
 		return (NULL);
+	i = count * size;
 	ptr = (char *)malloc(i);
 	if (ptr == NULL)
 		return (NULL);
